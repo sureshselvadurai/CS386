bba23f001d6cd60ceb3a032f8848970757c75904
sureshrajaselvadurai <sureshrajaselvadurai@gmail.com>
2023-09-20 17:08:05 -0700

    ICP 5

---
+++ Class_Notes/20-09-2023/test.js
@@ -0,0 +1,16 @@
+//let a=1;
+//let b=2;
+//let sum=a+b;
+//console.log(sum);
+//try{
+//    alert("Result : "+ sum);
+//}
+//catch{
+//    console.log("The sum : " + sum);
+//}
+
+let prompt1 = prompt("Enter number 1",0);
+let prompt2 = prompt("Enter number 2",0);
+let sum = prompt1+prompt2;
+console.log(sum);
+alert("The sum = "+sum)
---
+++ In-Class-Programming/icp5/icp5.js
@@ -0,0 +1,20 @@
+// Declare variables
+let op1 = 10.5;
+let op2 = 5.2;
+
+// Perform the addition of those two variables and store result in variable sum
+let sum = op1 + op2;
+
+// Perform the subtraction of those two variables and store result in variable diff
+let diff = op1 - op2;
+
+//Perform the division of those two variables and store result in variable quotient
+let quotient = op1 / op2;
+
+//Perform the multiplication of those two variables and store result in variable product
+let product = op1 * op2;
+
+console.log(`The sum of ${op1} and ${op2} = ${sum}`);
+console.log(`The difference of ${op1} and ${op2} = ${diff}`);
+console.log(`The quotient of ${op1} and ${op2} = ${quotient}`);
+console.log(`The product of ${op1} and ${op2} = ${product}`);
