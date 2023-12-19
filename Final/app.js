16fa95fe32249ad2edc28c4f6ec4c4d180cd40bc
sureshrajaselvadurai <sureshrajaselvadurai@gmail.com>
2023-12-18 18:44:15 -0800

    Final - refactor

--- Final/app.js
+++ Final/app.js
@@ -5,24 +5,50 @@ const config = require('./modules/config');
 const routes = require('./routes/routes');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const connectDB = require('./modules/db');
+const fs = require('fs');
 
 // Setting up handlebars
 app.set('view engine', 'hbs');
 app.engine(
   'hbs',
-  hbs.engine({
-    extname: 'hbs',
-    defaultLayout: 'mainLayout',
-    layoutsDir: __dirname + '/views/layouts',
-    partialsDir: __dirname + '/views/partials'
-  })
+    hbs({
+        extname: 'hbs',
+        defaultLayout: 'mainLayout',
+        layoutsDir: __dirname + '/views/layouts',
+        partialsDir: __dirname + '/views/partials'
+      })
 );
+
+//QueryDBSetup
+connectDB(true);
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//load and define model
+const querySchema = new mongoose.Schema({
+  message: String,
+  DateTime: String
+});
+const Query = mongoose.model('Query', querySchema);
 
 //Setup static
 app.use(express.static('public'));
 
 // Use routes by passing the 'app' instance
 routes(app);
+
+//routing
+app.post('/addQuery', (req, res) => {
+  const FormData = new Query(req.body);
+  FormData.DateTime = new Date().toLocaleString();
+  console.log(req.body);
+  console.log(FormData);
+  FormData.save().then(() => {
+    console.log('Data saved successfully.');
+    });
+});
 
 // Start the server
 app.listen(config.port, config.host, () => {
--- Final/routes/routes.js
+++ Final/routes/routes.js
@@ -1,27 +1,44 @@
 
 const path = require('path');
-
 const config = require('../modules/config');
+const readCSV = require('../modules/csvReader');
+const fs = require('fs');
+
+function renderPage(res, template, additionalData = {}) {
+  const data = {
+    appName: config.appName,
+    ...additionalData,
+  };
+  res.render(template, data);
+}
 
 module.exports = function (app) {
-
   app.get('/', (req, res) => {
-    res.render('main');
+    const postsData = readCSV('./private/data/posts.csv');
+    const sortedPostsData = postsData.sort((a, b) => new Date(a.timeAgo) - new Date(b.timeAgo));
+    renderPage(res, 'main', { posts: sortedPostsData });
   });
 
   app.get('/contact', (req, res) => {
-    res.render('contact');
+    renderPage(res, 'contact');
   });
 
   app.get('/profile', (req, res) => {
-    res.render('profile');
+    renderPage(res, 'profile');
   });
 
   app.get('/projects', (req, res) => {
-    res.render('projects');
+    renderPage(res, 'projects');
   });
 
   app.get('/resume', (req, res) => {
-    res.render('resume');
+    renderPage(res, 'resume');
   });
-}
+
+  app.get('/getpdf', (req, res) => {
+    const pdfPath = './private/data/Resume SureshSelvadurai.pdf';
+    const dataBuffer = fs.readFileSync(pdfPath);
+    const pdfText = dataBuffer.toString('base64');
+    res.json({ pdfText });
+  });
+};
